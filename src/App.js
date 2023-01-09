b6688a8b47b84796df44ba7a48951ea0d379e98e
Maria Chuikina <4uikina@gmail.com>
2023-01-09 22:28:02 +0100

    posts postslist

--- src/App.js
+++ src/App.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import ClassCounter from './components/ClassCounter';
-import Counter from './components/Counter';
+import React, { useState } from 'react';
+import './styles/App.css';
+import PostList from './components/PostList';
 
 function App() {
-    
+
+    const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript', body: 'Description'},
+    {id: 2, title: 'Javascript 2', body: 'Description'},
+    {id: 3, title: 'Javascript 3', body: 'Description'},    
+  ])
+
+    const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python', body: 'Description'},
+    {id: 2, title: 'Python 2', body: 'Description'},
+    {id: 3, title: 'Python 3', body: 'Description'},    
+  ])
+
   return (
     <div className="App">
-      <Counter />
-      <ClassCounter />   
-      
-      
-
+      <PostList posts = {posts} title="Список постов 1" />
+      <PostList posts = {posts2} title="Список постов 2" />
       
     </div>
-  );
+  ) 
+  
 }
 
 export default App;
