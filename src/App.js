134e7aaec91032b40c9b67dd8704e212f42a293b
Maria Chuikina <4uikina@gmail.com>
2023-01-12 16:24:06 +0100

    add select

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 
 function App() {
@@ -11,6 +12,10 @@ function App() {
     {id: 2, title: 'Javascript 2', body: 'Description'},
     {id: 3, title: 'Javascript 3', body: 'Description'},    
   ])
+
+  const [selectedSort, setSelectedSort] = useState('');
+
+
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -23,6 +28,19 @@ function App() {
   return (
     <div className="App">
       <PostForm create={createPost} />
+      <hr style={{margin: '15px 0'}} />
+      <div>
+        <MySelect
+          value={selectedSort}
+          onChange={sort => setSelectedSort(sort)} 
+          defaultValue="Сортировка по"
+          options={[
+            {value: 'title', name: 'По названию'},
+            {value: 'body', name: 'По описанию'},
+          ]}          
+          />
+          
+      </div>
       {posts.length
         ? <PostList remove={removePost} posts = {posts} title="Список постов 1" />    
         :
