0cde9d65c6c23a07702e4474ae73e44451caae66
Maria Chuikina <4uikina@gmail.com>
2023-01-26 10:03:54 +0100

    add comments for posts

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -9,15 +9,20 @@ const PostIdPage = () => {
 	const params = useParams() 
 	
 	const [post, setPost] = useState({})
-	const [fetchPostById, isLoading, error] = useFetching(async () => {
-		
+	const [comments, setComments] = useState([])
+	const [fetchPostById, isLoading, error] = useFetching(async () => {		
 		const response = await PostService.getById(params.id)
-		console.log(response.data)
 		setPost(response.data);
+	})
+
+	const [fetchComments, isComLoading, comError] = useFetching(async () => {		
+		const response = await PostService.getCommentsByPostId(params.id)
+		setComments(response.data);
 	})
 
 	useEffect(() => {
 		fetchPostById(params.id)
+		fetchComments(params.id)
 	}, [])
 
 	return(
@@ -25,11 +30,23 @@ const PostIdPage = () => {
 		<h1>POST  {params.id}</h1>
 		{isLoading
 			? <Loader />
-			: <div>{post.id}. {post.title}</div>
-			}
-		
+			:  <div>
+					<h2>{post.id}. {post.title}</h2>
+				</div>
+		}
+		<h2>Comments</h2>
+		{isComLoading
+			? <Loader />
+			: <div>
+				{comments.map(comm =>
+					<div style={{marginTop: 15}}>
+						<h5>{comm.name}</h5>
+						<div>{comm.body}</div>
+					</div>
+				)}
+			</div>
+		}		
 		</div>
 	)
-}
-
+	}
 export default PostIdPage;
